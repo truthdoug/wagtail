37f41f879ed99b3adcb7a04c41d3f16fe4641f2d
Karl Hobley <karl@torchbox.com>
2021-05-10 11:05:51 +0100

    Initial work on new sidebar react/css

--- client/src/custom.d.ts
+++ client/src/custom.d.ts
@@ -3,7 +3,20 @@ export {};
 declare global {
     interface Window { __REDUX_DEVTOOLS_EXTENSION__: any; }
 
+    // Wagtail globals
+
     interface WagtailConfig {
+        ADMIN_API: {
+            PAGES: string;
+            DOCUMENTS: string;
+            IMAGES: string;
+            EXTRA_CHILDREN_PARAMETERS: string;
+        };
+
+        ADMIN_URLS: {
+            PAGES: string;
+        };
+
         I18N_ENABLED: boolean;
         LOCALES: {
             code: string;
